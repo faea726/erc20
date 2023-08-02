f233dbe1af00f0aa014332273ceaa6b7e78171a5
faea726 <fantasy.eagle96@gmail.com>
2023-08-02 11:41:37 +0700

    check lp token

--- scripts/removeLiquidityETH.ts
+++ scripts/removeLiquidityETH.ts
@@ -1,24 +1,22 @@
 import { ethers } from "hardhat";
 require("dotenv").config();
 
-async function removeLiquidity() {
+async function removeLiquidityETH() {
   const [deployer, others] = await ethers.getSigners();
 
   const tkAddress = process.env.TOKEN_ADDRESS || "";
-  const router = await ethers.getContractAt(
-    "IRouter",
-    process.env.ROUTER_ADDRESS || ""
-  );
+  const routerAddress = process.env.ROUTER_ADDRESS || "";
+
+  const tk = await ethers.getContractAt("Token", tkAddress);
+  const router = await ethers.getContractAt("IRouter", routerAddress);
   const factory = await ethers.getContractAt(
     "IFactory",
     await router.factory()
   );
   const weth = await ethers.getContractAt("IETH", await router.WETH());
 
-  const pair = await ethers.getContractAt(
-    "IPair",
-    await factory.getPair(tkAddress, await weth.getAddress())
-  );
+  const pairAddress = await factory.getPair(tkAddress, await weth.getAddress());
+  const pair = await ethers.getContractAt("IPair", pairAddress);
 
   // Approve for router to use LP
   const lpBalance = await pair.balanceOf(deployer.address);
@@ -26,9 +24,24 @@ async function removeLiquidity() {
     console.log(`No LP for token${tkAddress}`);
     return;
   }
-  const approveTx = await pair.approve(await router.getAddress(), lpBalance);
-  await approveTx.wait();
-  console.log("Approved pair");
+
+  const totalSupply = await tk.totalSupply();
+  console.log("Checking amount token in liquidity...");
+  var lpToken: bigint;
+  while (true) {
+    lpToken = await tk.balanceOf(pairAddress);
+    if (lpToken <= (totalSupply * 19n) / 20n) {
+      break;
+    }
+    await new Promise((f) => setTimeout(f, 5000)); // recheck every 5s
+  }
+
+  const allow = await pair.allowance(deployer.address, routerAddress);
+  if (allow == 0n) {
+    const approveTx = await pair.approve(await router.getAddress(), lpBalance);
+    await approveTx.wait();
+    console.log("Approved pair");
+  }
 
   // Remove liquidity ETH
   const deadline = Math.round(Date.now() + 300);
@@ -46,7 +59,7 @@ async function removeLiquidity() {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-removeLiquidity().catch((error) => {
+removeLiquidityETH().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
